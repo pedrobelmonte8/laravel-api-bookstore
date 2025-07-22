9f884b4d077b229d35dc7dde27bcd43050266541
Pedro <pedrojuan36022@gmail.com>
2025-07-22 16:41:32 +0200

    Pequeño cambio en los Models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    protected $fillable = [
+        'title',
+        'author',
+        'published_date',
+        'isbn',
+        'category_id',
+    ];
+
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
     public function books()
     {
         return $this->hasMany(Book::class);
