06b9a1e18584d1a35e55d2da1cf1d8163b415cd4
Pedro <pedrojuan36022@gmail.com>
2025-07-22 18:26:35 +0200

    Configurado el archivo de rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\CategoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('books', BookController::class);
+Route::apiResource('categories', CategoryController::class);
