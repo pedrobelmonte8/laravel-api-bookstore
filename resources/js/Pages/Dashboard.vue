d81aefe8c83a51f303a1d86563832ecd70670277
Pedro <pedrojuan36022@gmail.com>
2025-07-22 23:58:04 +0200

    Añadido login y register

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+
+//Prueba carga de libros
+/* import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const books = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/books')
+    books.value = response.data
+  } catch (error) {
+    console.error('Error al cargar libros:', error)
+  }
+}) */
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('books', BookController::class);
-Route::apiResource('categories', CategoryController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('books', BookController::class);
+    Route::apiResource('categories', CategoryController::class);
+});
+
+/* Route::get('/books', [BookController::class, 'index']); 
+Route::post('/books', [BookController::class, 'store']); */
+
+/* Route::apiResource('books', BookController::class);
+Route::apiResource('categories', CategoryController::class); */
